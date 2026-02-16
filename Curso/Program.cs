a58fad9337cd6d21d03c9d2db284b6dd44db2521
André Luna <alm3@cin.ufpe.br>
2026-02-16 13:22:23 -0300

    Adds Main2 method to demonstrate applying ef core migrations programmatically

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,5 +1,33 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
 using System;
+using Microsoft.EntityFrameworkCore;
 
-Console.WriteLine("Hello, World!");
+namespace CursoEFCore;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("App Starts!");
+    }
+
+    // SET THE METHOD NAME AS MAIN2 FOR IT NOT TO BE CALLED AT APPLICATION START
+    static void Main2(string[] args)
+    {
+        // USUALLY MIGRATIONS ARE NOT APPLIED THIS WAY
+        // NOT RECOMMENDED IN PROD ENV
+
+        // THIS WAY MIGRATION WILL BE EXECUTEDF AT THE APP START 
+        // DB1 IS DISPOSED AT THE END OF MAIN
+        using var db1 = new Data.ApplicationContext();
+        db1.Database.Migrate();
+
+        // THIS WAY THE MIGRATIONS WILL BE EXECUTED AT THE APP START, AS WELL
+        // DB2 IS DISPOSED AT THE END OF THE BLOCK
+        using (var db2 = new Data.ApplicationContext())
+        {
+            db2.Database.Migrate();
+        }
+    }
+}
