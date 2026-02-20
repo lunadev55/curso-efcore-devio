968bf178939b8eecea6388298f5bd49ec2814051
André Luna <alm3@cin.ufpe.br>
2026-02-19 23:05:43 -0300

    Adds code with ways of adding an entity to the DB using EF Core

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -3,6 +3,9 @@
 using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
+// using CursoEFCore.Data;
 
 namespace CursoEFCore;
 
@@ -21,6 +24,40 @@ class Program
             // FOLLOW WITH BUSINESS RULE
         }
 
+        InserirDados();
+    }
+
+    private static void InserirDados()
+    {
+        var produto = new Produto
+        {
+            CodigoBarras = "01234567891011",
+            Descricao = "Seda Papelito - King Size",
+            Valor = 12.23M,
+            TipoProduto = TipoProduto.MercadoriaParaRevenda,
+            Ativo = true
+        };
+
+        using var db = new Data.ApplicationContext();
+
+        // FIRST WAY I WROTE        
+        db.Produtos.Add(produto);
+
+        // SECOND APPROACH
+        db.Set<Produto>().Add(produto);
+
+        // THIRD APPROACH - TRACKING THE ENTITY STATE
+        db.Entry(produto).State = EntityState.Added;
+
+        // NEEDS TO KNOW THE ENTITY UNDER THE HOOD
+        db.Add(produto);
+
+        var saveChanges = db.SaveChanges();
+
+        if (saveChanges > 0)
+        {
+            Console.WriteLine("Entity Saved Succesfully!");
+        }
     }
 
     // SET THE METHOD NAME AS MAIN2 FOR IT NOT TO BE CALLED AT APPLICATION START
